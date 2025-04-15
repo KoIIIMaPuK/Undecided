39f40b86094f1ff978b602c6e4441e5148bbf67c
dasha <names2007@mail.ru>
2025-04-15 22:59:18 +0300

    create a class AstronomicalObjects and basic interface

--- pls-give-me-a-name/AstronomicalObject.h
+++ pls-give-me-a-name/AstronomicalObject.h
@@ -1,12 +1,51 @@
 #pragma once
 
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+//		CPP Lib
+//
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#include <iostream>
+
+
+
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+//		SFML lib
+//
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#include <SFML/Graphics.hpp>
+
+
+
+
+
 
 class AstronomicalObject
 {
-protected:
-	float mass;
-	float radius;
-	float position;
-	float velocity;
-};
+public:
+	AstronomicalObject(uint8_t size, sf::Color color, sf::Vector2f position, sf::Vector2f velocity, float mass);
+	
+	void setPosition(sf::Vector2f position);
+	void setVelocity(sf::Vector2f velocity);
+	void setAcceleration(sf::Vector2f Acceleration);
+	void setMass(float mass);
+	
+	sf::Vector2f getPosition() const;
+	sf::Vector2f getVelocity() const;
+	sf::Vector2f getAcceleration() const;
+	float getMass() const;
 
+	void update();
+	void draw(sf::RenderWindow& window) const;
+
+
+protected:
+	sf::CircleShape body;
+	sf::Vector2f position;
+	sf::Vector2f velocity;
+	sf::Vector2f acceleration;
+	
+	float mass;
+};
